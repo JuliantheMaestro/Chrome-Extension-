75c96c8192d806074ad766eb61bbf9883c9f4a7c
Jewlun <julian.boehnertkrueger@gmail.com>
2024-11-04 23:05:21 -0500

    Learned about template strings and added that to make it easier to read

--- script.js
+++ script.js
@@ -12,8 +12,13 @@ renderLeads()
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEl.innerHTML = listItems
-
+    ulEl.innerHTML = listItems  
 }
